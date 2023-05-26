bbed890e67731e1ed21ba43147f5ec2cdddf8568
Diyeipipe <diazrochajuanfe@hotmail.com>
2023-05-25 20:59:55 -0500

    login tests

--- tests/users.test.js
+++ tests/users.test.js
@@ -12,8 +12,31 @@ describe('Users endpoint tests', () => {
             const body = response._body
 
             expect(body.status).to.equal('success')
-            expect(body.payload).to.have.property('email')
             expect(body.payload).to.have.property('role')
+        }).timeout(5000)
+    })
+
+    describe('Login', () => {
+        it('Login ok', async() => {
+            const user = {
+                email:"a",
+                password:"a"
+            }
+            const response = await requester.post(`/api/session/login`).send(user)
+            const body = response._body
+
+            expect(body.status).to.equal('success')
+        })
+
+        it('Login failed', async() => {
+            const user = {
+                email:"a",
+                password:"b"
+            }
+            const response = await requester.post(`/api/session/login`).send(user)
+            const body = response._body
+
+            expect(body.error).to.equal('email or password invalid')
         })
     })
 })
